c355c8986301599172486350a33b03c41c010320
Lukas <HLK65@users.noreply.github.com>
2019-10-19 23:42:43 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         AutoClaim
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  automatically claim twitch bonus points
+// @author       hlk
+// @include      https://www.twitch.tv/*
+// @include      https://twitch.tv/*
+// @exclude      https://www.twitch.tv/*/*
+// @exclude      https://twitch.tv/*/*
+// @grant        none
+// ==/UserScript==
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function find() {
+    if (document.getElementsByClassName('claimable-bonus__icon').length !== 0) {
+      console.log('AutoClaim: Found claimable bonus.');
+      await sleep(200);
+      document.getElementsByClassName('claimable-bonus__icon')[0].firstElementChild.click()
+      await sleep(5*60*1000)
+    }
+    else {
+      //console.log('AutoClaim: 404 keep looking.');
+    }
+}
+
+
+var intervalId = window.setInterval(find, 5000);
